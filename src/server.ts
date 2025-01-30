6de7b51ec1a529facb342fec79cb57863066fe64
Gabriel Raposo <gabriel.henrique7@hotmail.com>
2025-01-30 15:25:20 -0300

    feat(server): remover configurações de ambiente e ícones
    - Deletado o arquivo .env.exemple que continha configurações de ambiente.
    - Removidas as importações e o uso de ícones no arquivo src/server.ts.
    - Eliminadas as referências ao logo e favicon no registro do fastifySwaggerUI.
    
    Essas mudanças simplificam a configuração do ambiente e reduzem a complexidade do código, melhorando a manutenção e a clareza do projeto.

--- src/server.ts
+++ src/server.ts
@@ -7,14 +7,6 @@ import fastifyCors from "@fastify/cors";
 import { env } from "@/env";
 import { errorHandler } from "./error-handler";
 import { createAccount } from "./routes/auth/create-account";
-import { join } from "node:path";
-import { readFileSync } from "node:fs";
-
-const _logo = join(__dirname, "assets", "logoExemplo.png");
-const _favIcon = join(__dirname, "assets", "logoExemplo.png");
-
-const logo = readFileSync(_logo);
-const favIcon = readFileSync(_favIcon);
 
 const app = fastify({
   logger: env.NODE_ENV == "dev" ? true : false,
@@ -47,23 +39,6 @@ app.register(fastifySwagger, {
 
 app.register(fastifySwaggerUI, {
   routePrefix: "/docs",
-  logo: {
-    type: "image/png",
-    content: logo,
-    href: "/docs",
-    target: "_blank",
-  },
-  theme: {
-    favicon: [
-      {
-        filename: "favicon.png",
-        rel: "icon",
-        sizes: "16x16",
-        type: "image/png",
-        content: favIcon,
-      },
-    ],
-  },
 });
 
 app.register(fastifyJwt, {
